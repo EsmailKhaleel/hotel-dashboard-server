7154eb6c321fbb95d46941cd9f405a76580d07bd
EsmailKhaleel <esmailkhaleel27@gmail.com>
2025-06-26 03:10:47 +0300

    Update CORS configuration to allow requests from Netlify domain

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://mern-hotel-admin-dashboard.netlify.app/"],
   credentials: true,
 }));
 app.use(morgan('dev'));
