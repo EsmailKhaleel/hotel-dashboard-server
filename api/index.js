52ffe349c11990f32e19473c7fde2a78ff70b134
EsmailKhaleel <esmailkhaleel27@gmail.com>
2025-06-27 12:08:47 +0300

    Refactor server startup: move MongoDB connection into startServer function for better error handling

--- api/index.js
+++ api/index.js
@@ -16,8 +16,7 @@ const {
   errorHandler,
 } = require("../src/middleware/error.middleware");
 
-// Connect to MongoDB
-connectDB();
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -72,16 +71,17 @@ app.get("/", (req, res) => {
 app.use(notFound);
 app.use(errorHandler);
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({
-    status: "error",
-    message: "Something broke!",
-    error: err.message,
-  });
-});
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB(); 
+    app.listen(PORT, () => {
+      console.log(`Server is running at http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error Failed to start server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
